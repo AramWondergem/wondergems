30d18a1eb5118e2539345f022d5e55c425f8f154
AramWondergem <aramwondergem@gmail.com>
2024-08-08 14:56:16 -0400

    feat: header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,6 @@
 import { component$ } from '@builder.io/qwik';
+import styles from './Header.module.css';
+
 
 import { MoMenu } from '@qwikest/icons/monoicons';
 
@@ -8,15 +10,20 @@ export interface HeaderProps {
 
 export const Header = component$<HeaderProps>((props) => {
   return (
-      <>
-      <div className="outerbox">
-          <div className="innerbox">
-              <div>
-                  <span>ARAM</span>
-                  <span>WONDERGEM</span>
+      < >
+      <div class={[styles.headerOuterbox, 'outerbox']} >
+          <div class={[styles.headerInnerbox ,'innerbox']}>
+              <div class={['hideOnDesktop', styles.headerSpacer, styles.headerMenu]}>
+                  <MoMenu/>
               </div>
               <div>
-                  <MoMenu/>
+                  <h1 class={[styles.headerLogo]}><span>ARAM</span> <span class={[styles.headerLogoHide]}>WONDERGEM</span></h1>
+              </div>
+              <div class={['hideOnMobile']}>
+                  Navlinks
+              </div>
+              <div class={['hideOnDesktop', ]}>
+                  <MoMenu class={[styles.headerMenu]}/>
               </div>
           </div>
       </div>
