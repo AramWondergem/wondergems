3e98888def2f96543da2e337b46389790195c704
AramWondergem <aramwondergem@gmail.com>
2024-08-21 16:03:16 -0400

    feat: button

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,29 @@
-import {component$} from "@builder.io/qwik";
+import {component$, $} from "@builder.io/qwik";
 import type {DocumentHead} from "@builder.io/qwik-city";
 import {Heading} from "~/components/Heading/Heading";
+import {Button} from "~/components/Button/Button";
+import styles from "./Index.module.css"
 
 
 export default component$(() => {
+    const onClick = $(() => alert('Hallo hier ben ik'));
+
     return (
-        <div class={'flexgrow'}>
+        <div class={['flexgrow', 'outerbox', styles.outerboxPage]}>
             <Heading  bgColor={'rose'}>
                 <h2>ABOUT</h2>
             </Heading>
+            <div class={['innerbox', styles.innerBoxPage]}>
+
             <Heading  bgColor={'rose'}>
                 <h3>TECHNOLOGY</h3>
             </Heading>
             <Heading  bgColor={'rose'}>
                 <h4>ABOUT</h4>
             </Heading>
+
+            <Button onClick={onClick}>See my projects</Button>
+            </div>
         </div>
     );
 });
