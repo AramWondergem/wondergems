40f13389a1887f70d23f459d151d3f003b3d7c3b
AramWondergem <aramwondergem@gmail.com>
2024-09-10 14:10:57 -0400

    feat: added automated code linting

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,16 @@
 import { component$ } from '@builder.io/qwik';
-import  styles  from './Footer.module.css'
+import styles from './Footer.module.css';
 
-export interface FooterProps {
-
-}
+export interface FooterProps {}
 
 export const Footer = component$<FooterProps>(() => {
-  return (
-    <div class={[styles.footerOuterbox, 'outerbox']}>
-      <div class={[styles.footerInnerbox, 'innerbox']}>
-          <footer class={[styles.footerText]}><small>COPYRIGHT © {new Date().getFullYear()} ARAM WONDERGEM</small></footer>
-      </div>
-    </div>
-  );
+    return (
+        <div class={[styles.footerOuterbox, 'outerbox']}>
+            <div class={[styles.footerInnerbox, 'innerbox']}>
+                <footer class={[styles.footerText]}>
+                    <small>COPYRIGHT © {new Date().getFullYear()} ARAM WONDERGEM</small>
+                </footer>
+            </div>
+        </div>
+    );
 });
